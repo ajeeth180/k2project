79bcf8cc8ba6b7b550bfc10e6a37b03d10302169
DESKTOP-J8873NK\Ajeeth <ajeeth180@gmail.com>
2018-05-24 15:16:19 +0200

    Created home page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,44 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/aanbod", name="aanbod")
+     */
+    public function aanbodAction(Request $request)
+    {
+        return $this->render('default/aanbod.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request)
+    {
+        return $this->render('default/register.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/gedrag", name="gedrag")
+     */
+    public function gedragAction(Request $request)
+    {
+        return $this->render('default/gedrag.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contractAction(Request $request)
+    {
+        return $this->render('default/contact.html.twig', [
         ]);
     }
 }
