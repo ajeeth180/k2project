07c0504ddf957c9a7162dc41a343376cd539b71a
DESKTOP-J8873NK\Ajeeth <ajeeth180@gmail.com>
2018-05-31 12:12:20 +0200

    ik heb niks gedaan

--- src/AppBundle/Controller/MemberController.php
+++ src/AppBundle/Controller/MemberController.php
@@ -1,15 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Amaru Signore
- * Date: 24-5-2018
- * Time: 14:55
- */
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class MemberController
+class MemberController extends Controller
 {
+    /**
+     * @Route("/member/", name="userHomepage")
+     */
+    public function userHomeAction()
+    {
+        return $this->render('member/homepage.html.twig');
+    }
 
 }
